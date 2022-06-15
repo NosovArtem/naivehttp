dc26d6cf135df86d73ab32b9eaa028c73bc6106f
Artem <nosov.a.o@yandex.ru>
2022-06-15 15:59:25 +0300

    [Start] simple http server #2.1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        new Server(null).bootstrap();
+        new Server(((request, response) -> {
+            return "<html><body><h1>Hello, client</h1>It handler</body></html>";
+        })).bootstrap();
     }
 }
 
